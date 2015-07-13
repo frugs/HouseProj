c5faca1d667ca70fe6a29fc87eb6cfebf6d704af
Hugo Wainwright <wainwrighthugo@gmail.com>
2015-07-13 17:51:42 +0100

    Added score display

---
+++ Assets/Scripts/UI/ScoreDisplayBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI {
+    [RequireComponent(typeof(Text))]
+    public class ScoreDisplayBehaviour : MonoBehaviour {
+        [SerializeField]
+        private ScoreBehaviour _score;
+
+        private Text _text;
+
+        public void Awake() {
+            _text = GetComponent<Text>();
+        }
+
+        public void LateUpdate() {
+            _text.text = _score.Score.ToString();
+        }
+    }
+}
