fa1e0aa5379e61c9e1b73b4acde014ebb62a3492
Hugo Wainwright <wainwrighthugo@gmail.com>
2015-07-14 01:27:31 +0100

    Added full screen score display before restarting level on death

---
+++ Assets/Scripts/FullscreenScoreDisplayBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts {
+    
+    [RequireComponent(typeof(Text))]
+    public class FullscreenScoreDisplayBehaviour : MonoBehaviour {
+        [SerializeField]
+        private ScoreBehaviour _scoreBehaviour;
+        
+        private Text _text;
+
+        public void Awake() {
+            _text = GetComponent<Text>();
+        }
+        
+        public void Update() {
+            _text.text = _scoreBehaviour.Score + " fish!";
+        }
+    }
+}
---
+++ Assets/Scripts/RestartLevelBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using Assets.Scripts.UI;
+using UnityEngine;
+
+namespace Assets.Scripts {
+    [RequireComponent(typeof(Collider2D))]
+    public class RestartLevelBehaviour : MonoBehaviour {
+        [SerializeField]
+        private ScoreDisplayBehaviour _scoreDisplay;
+
+        [SerializeField]
+        private FullscreenScoreDisplayBehaviour _fullscreenScoreDisplay;
+        
+        public void OnTriggerEnter2D(Collider2D other) {
+            if (other.tag == "Player") {
+                StartCoroutine(RestartLevel());
+            }
+        }
+
+        private IEnumerator RestartLevel() {
+            _scoreDisplay.gameObject.SetActive(false);
+            _fullscreenScoreDisplay.gameObject.SetActive(true);
+            yield return new WaitForSeconds(1.5f);
+            Application.LoadLevel(Application.loadedLevelName);
+        }
+    }
+}
