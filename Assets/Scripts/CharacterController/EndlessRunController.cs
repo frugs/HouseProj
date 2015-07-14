cd9dcae31ef219a719bbcd7fb872240d9ef7cb1c
Hugo Wainwright <wainwrighthugo@gmail.com>
2015-07-14 21:38:47 +0100

    Cleaned up controls

--- Assets/Scripts/CharacterController/EndlessRunController.cs
+++ Assets/Scripts/CharacterController/EndlessRunController.cs
@@ -23,7 +23,7 @@ namespace Assets.Scripts.Character
 			if (!m_Jump)
 			{
 				// Read the jump input in Update so button presses aren't missed.
-				m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+			    m_Jump = Input.GetButtonDown("Jump");
 			}
 		}
 		
@@ -31,7 +31,7 @@ namespace Assets.Scripts.Character
 		private void FixedUpdate()
 		{
 			// Read the inputs.
-			bool crouch = Input.GetKey(KeyCode.LeftControl);
+		    bool crouch = Input.GetButton("Crouch");
 			m_Character.ApplyControl(m_RunForce, crouch, m_Jump);
 			m_Jump = false;
 		}
