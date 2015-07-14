3bcd3550dc24369125e0da4803f3e7a9737eaf48
Hugo Wainwright <wainwrighthugo@gmail.com>
2015-07-14 01:41:22 +0100

    Refactored level generator a bit, extracted section generation into another class

--- Assets/Scripts/LevelGeneratorBehaviour.cs
+++ Assets/Scripts/LevelGeneratorBehaviour.cs
@@ -1,29 +1,21 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Specialized;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 namespace Assets.Scripts {
     public class LevelGeneratorBehaviour : MonoBehaviour {
-        private enum Section {
-            Ground,
-            SmallGap,
-            LargeGap
-        }
-
         // TODO: This is pretty hacky and questionable
         private const float PlatformWidth = 2f;
         private const float LargeGapMultiplier = 1.75f;
-        private const float FishParabolaHeight =2f;
-
+        private const float FishParabolaHeight = 2f;
 
         private static readonly IDictionary<Section, float> SectionWeights = new Dictionary<Section, float> {
             {Section.Ground, 4f},
             {Section.SmallGap, 1f},
             {Section.LargeGap, 2f}
         };
+
+        private readonly SectionGenerator _sectionGenerator = new SectionGenerator(SectionWeights);
 
         [SerializeField]
         private ScoreBehaviour _scoreBehaviour;
@@ -42,15 +34,15 @@ namespace Assets.Scripts {
 
         // TODO: This doesn't end up being the actual section count
         [SerializeField]
-        private readonly int _sectionCount = 100;
+        private int _sectionCount = 100;
 
         [SerializeField]
-        private readonly float _gapSize = 2.5f;
+        private float _gapSize = 2.5f;
 
         public void Start() {
             var previousSection = Section.SmallGap;
             var progress = 0f;
-            var sections = GenerateSections(_sectionCount);
+            var sections = _sectionGenerator.GenerateSections(_sectionCount);
             foreach (var section in sections) {
                 // TODO: This is pretty ugly, revisit
                 switch (previousSection) {
@@ -106,34 +98,6 @@ namespace Assets.Scripts {
             }
         }
 
-        private static IEnumerable<Section> GenerateSections(int sectionCount) {
-            var cumulativeSectionWeights = new OrderedDictionary();
-            var cumulativeWeight = 0f;
-            foreach (var pair in SectionWeights) {
-                cumulativeWeight += pair.Value;
-                cumulativeSectionWeights.Add(pair.Key, cumulativeWeight);
-            }
-
-            var sections = new List<Section> {Section.Ground};
-            for (var i = 0; i < sectionCount; i++) {
-                foreach (var entry in cumulativeSectionWeights) {
-                    var dictionaryEntry = (DictionaryEntry) entry;
-                    if (Random.Range(0f, cumulativeWeight) <= (float) dictionaryEntry.Value) {
-                        var section = (Section) dictionaryEntry.Key;
-                        sections.Add(section);
-
-                        if (section == Section.SmallGap || section == Section.LargeGap) {
-                            sections.Add(Section.Ground);
-                        }
-                        break;
-                    }
-                }
-            }
-            sections.Add(Section.SmallGap);
-
-            return sections;
-        }
-
         private void CreateFishOnPlatform(float progress) {
             var fish1Position = new Vector2(transform.position.x + progress + (PlatformWidth / 3),
                                             transform.position.y + 1f);
@@ -145,7 +109,7 @@ namespace Assets.Scripts {
         }
 
         private void CreateFishParabola(float progress, float parabolaWidth, float parabolaHeight) {
-            float halfWidthSquared = (parabolaWidth / 2) * (parabolaWidth / 2);
+            var halfWidthSquared = (parabolaWidth / 2) * (parabolaWidth / 2);
             Func<float, float> parabola = x => x * (parabolaWidth - x) * (parabolaHeight / halfWidthSquared);
             var origin = transform.position.x + progress;
 
---
+++ Assets/Scripts/SectionGenerator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+namespace Assets.Scripts {
+    public enum Section {
+        Ground,
+        SmallGap,
+        LargeGap
+    }
+
+    public class SectionGenerator {
+        private readonly IDictionary<Section, float> _sectionWeights;
+
+        public SectionGenerator(IDictionary<Section, float> sectionWeights) {
+            _sectionWeights = sectionWeights;
+        }
+
+        public IEnumerable<Section> GenerateSections(int sectionCount) {
+            var cumulativeSectionWeights = new OrderedDictionary();
+            var cumulativeWeight = 0f;
+            foreach (var pair in _sectionWeights) {
+                cumulativeWeight += pair.Value;
+                cumulativeSectionWeights.Add(pair.Key, cumulativeWeight);
+            }
+
+            var sections = new List<Section> {Section.Ground};
+            for (var i = 0; i < sectionCount; i++) {
+                foreach (var entry in cumulativeSectionWeights) {
+                    var dictionaryEntry = (DictionaryEntry) entry;
+                    if (Random.Range(0f, cumulativeWeight) <= (float) dictionaryEntry.Value) {
+                        var section = (Section) dictionaryEntry.Key;
+                        sections.Add(section);
+
+                        if (section == Section.SmallGap || section == Section.LargeGap) {
+                            sections.Add(Section.Ground);
+                        }
+                        break;
+                    }
+                }
+            }
+            sections.Add(Section.SmallGap);
+
+            return sections;
+        }
+    }
+}
