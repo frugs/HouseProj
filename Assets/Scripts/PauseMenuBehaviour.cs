f2e00e9bcbc3576a3e88b6420603405b7ae71f08
Hugo Wainwright <hwainwright@gmx.com>
2015-07-15 01:59:58 +0100

    Added pause menu to allow user to quit game

---
+++ Assets/Scripts/PauseMenuBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts {
+    public class PauseMenuBehaviour : MonoBehaviour {
+        [SerializeField]
+        private GameObject _menu;
+
+        private bool _paused;
+
+        public void Update() {
+            if (Input.GetButtonDown("Cancel")) {
+                _paused = !_paused;
+                UpdatePaused();
+            }
+        }
+
+        //Called from menu button
+        public void Resume() {
+            _paused = false;
+            UpdatePaused();
+        }
+
+        // Called from menu button
+        public void Quit() {
+            Application.Quit();
+        }
+
+        private void UpdatePaused() {
+            Time.timeScale = _paused ? 0f : 1f;
+            _menu.SetActive(_paused);
+        }
+    }
+}
