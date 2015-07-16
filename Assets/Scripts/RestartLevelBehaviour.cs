45f5770f8891bf2906260ef9577fafb130d97818
Hugo Wainwright <wainwrighthugo@gmail.com>
2015-07-16 23:55:25 +0100

    Player can now skip high score display timeout by pressing jump or touching the screen

--- Assets/Scripts/RestartLevelBehaviour.cs
+++ Assets/Scripts/RestartLevelBehaviour.cs
@@ -5,26 +5,44 @@ using UnityEngine;
 
 namespace Assets.Scripts {
     [RequireComponent(typeof (Collider2D))]
-    public class RestartLevelBehaviour : MonoBehaviour {
+    public class
+            RestartLevelBehaviour : MonoBehaviour {
         public ScoreDisplayBehaviour ScoreDisplayBehaviour;
         public FullscreenScoreDisplayBehaviour FullscreenScoreDisplayBehaviour;
 
         public void OnTriggerEnter2D(Collider2D other) {
             if (other.tag == "Player") {
                 other.GetComponent<EndlessRunCharacter>().Kill();
-                StartCoroutine(RestartLevel());
+                StartCoroutine(ShowHighScoreCoroutine());
             }
         }
 
-        private IEnumerator RestartLevel() {
+        private static void RestartLevel() {
+            Application.LoadLevel(Application.loadedLevelName);
+        }
+
+        private IEnumerator ShowHighScoreCoroutine() {
             yield return new WaitForSeconds(0.5f);
             ScoreDisplayBehaviour.gameObject.SetActive(false);
             FullscreenScoreDisplayBehaviour.gameObject.SetActive(true);
-            while (!FullscreenScoreDisplayBehaviour.IsDone ()) {
-				yield return null;
-			}
-			yield return new WaitForSeconds (4);
-            Application.LoadLevel(Application.loadedLevelName);
+            while (!FullscreenScoreDisplayBehaviour.IsDone()) {
+                yield return null;
+            }
+
+            StartCoroutine(RestartLevelOnTimerCoroutine());
+            StartCoroutine(RestartLevelOnUserActionCoroutine());
+        }
+
+        private static IEnumerator RestartLevelOnTimerCoroutine() {
+            yield return new WaitForSeconds(4);
+            RestartLevel();
+        }
+
+        private IEnumerator RestartLevelOnUserActionCoroutine() {
+            while (!Input.GetButtonDown("Jump") && Input.touchCount == 0) {
+                yield return null;
+            }
+            RestartLevel();
         }
     }
 }
