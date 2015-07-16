69ae879053ac876638253df6d4aeee4283ce9855
Hugo Wainwright <wainwrighthugo@gmail.com>
2015-07-17 00:05:46 +0100

    Focus name entry field automatically on new highscore

---
+++ Assets/Scripts/UI/FocusOnEnableInputFieldBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI {
+    [RequireComponent(typeof (InputField))]
+    public class FocusOnEnableInputFieldBehaviour : MonoBehaviour {
+        public void OnEnable() {
+            GetComponent<InputField>().ActivateInputField();
+        }
+    }
+}
